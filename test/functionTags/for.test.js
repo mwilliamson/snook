97c7a119c5448e7693e4c413b677a71062483d9a
Michael Williamson <mike@zwobble.org>
2012-08-26 17:06:34 +0100

    Add implementation for "for"

--- test/functionTags/for.test.js
+++ test/functionTags/for.test.js
@@ -9,13 +9,26 @@ var staticContext = {
 
 exports["for does nothing if collection is empty"] = function(test) {
     var reader = createReader({
-        page: "{#for name in names}{name}{/name}"
+        page: "{#for name in names}* {name}\n{/for}"
     });
     
     var templates = new templating.Templates(reader, staticContext);
     templates.render("page", {names: []})
         .then(function(output) {
             test.equal('', output);
+            test.done();
+        }).end();
+};
+
+exports["for iterates over every element in array"] = function(test) {
+    var reader = createReader({
+        page: "{#for name in names}* {name}\n{/for}"
+    });
+    
+    var templates = new templating.Templates(reader, staticContext);
+    templates.render("page", {names: ["Bob", "Jim"]})
+        .then(function(output) {
+            test.equal('* Bob\n* Jim\n', output);
             test.done();
         }).end();
 };
