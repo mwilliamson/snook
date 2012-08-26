857e2da2c9ef0cb01b01c77a69fdf9c23bd345d5
Michael Williamson <mike@zwobble.org>
2012-08-26 16:33:15 +0100

    Split up function tag definitions

--- test/functionTags/include.test.js
+++ test/functionTags/include.test.js
@@ -1,7 +1,13 @@
 var q = require("q");
 
-var staticContexts = require("../lib/staticContexts");
-var templating = require("../lib/templating");
+var templating = require("../../lib/templating");
+var include = require("../../lib/functionTags").include;
+var hole = require("../../lib/functionTags").hole;
+
+var staticContext = {
+    include: include,
+    hole: hole
+};
 
 exports["include function renders referenced template"] = function(test) {
     var reader = createReader({
@@ -9,7 +15,7 @@ exports["include function renders referenced template"] = function(test) {
         nav: '<a href="/">Home</a>'
     });
     
-    var templates = new templating.Templates(reader, staticContexts.create);
+    var templates = new templating.Templates(reader, staticContext);
     templates.render("page", {})
         .then(function(output) {
             test.equal('<a href="/">Home</a> <h1>Store</h1>', output);
@@ -23,7 +29,7 @@ exports["include function creates empty context for included template"] = functi
         nav: '<a href="/">{name}</a>'
     });
     
-    var templates = new templating.Templates(reader, staticContexts.create);
+    var templates = new templating.Templates(reader, staticContext);
     templates.render("page", {name: "Home"})
         .then(function(output) {
             test.equal('<a href="/"></a> <h1>Store</h1>', output);
@@ -37,7 +43,7 @@ exports["include function can use context set by named argument"] = function(tes
         nav: '<a href="/">{name}</a>'
     });
     
-    var templates = new templating.Templates(reader, staticContexts.create);
+    var templates = new templating.Templates(reader, staticContext);
     templates.render("page", {project: {name: "Home"}})
         .then(function(output) {
             test.equal('<a href="/">Home</a> <h1>Store</h1>', output);
@@ -51,7 +57,7 @@ exports["include function can fill in holes"] = function(test) {
         nav: '{#hole title /}<a href="/">Home</a>'
     });
     
-    var templates = new templating.Templates(reader, staticContexts.create);
+    var templates = new templating.Templates(reader, staticContext);
     templates.render("page", {})
         .then(function(output) {
             test.equal('<h1>Store</h1><a href="/">Home</a>', output);
